d01fd908e20b14332a6820d3b88942dc12eec316
Srajan Rai <mesrajanrai@gmail.com>
2022-11-13 20:02:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 void main() {
     system("cls");
-    printf("Hello World");
+    // printf("Hello World");
+
+    char name[10], surname[10];
+    scanf("%s %s", name, surname);
+
+    printf("Hello %s %s", name, surname);
+
     return;
 }
